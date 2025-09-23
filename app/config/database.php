7c7c3a2bfdf26232e7b62e39b265ef9013197878
Ara Villena <aragracevillena@gmail.com>
2025-09-23 09:39:12 +0330

    m

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798632',
-    'password'	=> 'RaUzZesG4N',
-    'database'	=> 'sql12798632',
+    'username'	=> 'sql12798038',
+    'password'	=> 'pRye6fENI6',
+    'database'	=> 'sql12798038',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
